2091b4a7e5a63df03e1d48e7f8b6410a64241894
pianzub <pianzub@gmail.com>
2026-09-21 19:06:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
   // Tempel URL deployment Google Apps Script yang berakhiran /exec.
-  API_URL: 'PASTE_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwpTU7OST6RTTbMxm5B_JTY9QzSJeZsvhTEVjW7Flvhv-BPBvACWX-P8p4-SW694MAa1w/exec'
 });
